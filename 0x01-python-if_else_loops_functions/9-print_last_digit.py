94a5005dc3b03a2a90612578a4b3bf8ef343006b
your_github_username <richesuche91@gmail.com>
2023-11-28 18:45:22 -0800

    if/else, loops, functions

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if number >= 0:
-        i_digit = number % 10
+        l_digit = number % 10
     else:
         l_digit = number % -10
         l_digit *= -1
