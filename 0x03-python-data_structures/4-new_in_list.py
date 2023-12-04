dfd97ff6c6c3d7bd6810580655de06f7efbee8fe
your_github_username <richesuche91@gmail.com>
2023-12-04 15:10:46 -0800

    Data Structures: Lists, Tuples

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    length = len(my_list)
+    if (idx < 0) or (idx > (len(my_list)-1)):
+        return my_list
 
-    new_list = my_list[:]
-
-    if 0 <= idx < length:
-        new_list[idx] = element
-
-    return (my_list)
+    copy = [x for x in my_list]
+    copy[idx] = element
+    return copy
