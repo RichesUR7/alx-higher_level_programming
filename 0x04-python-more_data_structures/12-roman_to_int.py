13f61364d814dfec5a6b341ea3a80e34f5e51cab
your_github_username <richesuche91@gmail.com>
2023-12-06 18:11:19 -0800

    More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python3
+
 def roman_to_int(roman_string):
-    if roman_string is None or not isinstance(roman_string, str):
+    if not isinstance(roman_string, str) or roman_string is None:
         return 0
-    rv = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    result = 0
-    for i in range(len(roman_string)):
-        if i > 0 and rv[roman_string[i]] > rv[roman_string[i - 1]]:
-            result += rv[roman_string[i]] - 2 * rv[roman_string[i - 1]]
+
+    roman_numerals = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50,
+        'C': 100, 'D': 500, 'M': 1000
+    }
+
+    total = 0
+    prev_value = 0
+
+    for char in reversed(roman_string):
+        current_value = roman_numerals[char]
+        if current_value >= prev_value:
+            total += current_value
         else:
-            result += rv[roman_string[i]]
-        return result
+            total -= current_value
+        prev_value = current_value
+
+    return total
