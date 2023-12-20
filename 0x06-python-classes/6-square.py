e3b01165e6c0201f8d2d09ac061670f9ac7b2c9e
your_github_username <richesuche91@gmail.com>
2023-12-20 07:30:59 -0800

    Classes and Objects

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -51,7 +51,7 @@ class Square:
             return
 
         for _ in range(self.__position[1]):
-            print("\n" * self.__position[1], end="")
+            print("")
         for _ in range(self.__size):
             print(" " * self.__position[0] + "#" * self.__size)
 
