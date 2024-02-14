6acb9f91a1d3e7e91cf20de693b45ad1857c7ca1
your_github_username <richesuche91@gmail.com>
2024-02-14 15:56:20 -0800

    SQL

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,4 @@
--- Create user user_0d_1 if it does not exist
+-- Script that creates an user in MySQL server
+-- Query to create the user 'user_0d_1' in MySQL server
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
--- Grant all privileges to user_0d_1
-GRANT ALL PRIVILEGES ON . TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON . TO user_0d_1@localhost;
